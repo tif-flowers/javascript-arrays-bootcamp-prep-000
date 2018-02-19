f533f0e99d6975242b584ae188a1d44d0b40bbdd
Tif Flowers <tif@gentlebull.com>
2018-02-19 19:04:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,4 +33,6 @@ function removeElementFromBeginningOfArray(array) {
   
 }
 
-function destructivelyRemoveElementFromEndOfArray(array)
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1)
+}
