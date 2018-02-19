d5a8bc6c9c1101a69e53b4e93de5f7bb93fe85c7
Tif Flowers <tif@gentlebull.com>
2018-02-19 18:52:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+}
